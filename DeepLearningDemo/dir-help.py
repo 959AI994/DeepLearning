41eac2e687b27b53aee8afc61dac6f7ddba543bb
959AI994 <115469462+959AI994@users.noreply.github.com>
2024-11-07 11:13:06 +0800

    feat: fix code

--- DeepLearningDemo/dir-help.py
+++ DeepLearningDemo/dir-help.py
@@ -1,8 +1,8 @@
 import torch
 print(torch.cuda.is_available())
-### 看当前pytorch中有哪些工具箱
+# 看当前pytorch中有哪些工具箱
 print(dir(torch))
-### 看一下某个工具箱中有哪些工具
+# 看一下某个工具箱中有哪些工具
 print(dir(torch.cuda))
-### 看一下工具怎么使用
+# 看一下工具怎么使用
 print(help(torch.cuda.is_available))
