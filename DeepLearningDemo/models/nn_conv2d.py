0f13af7e9265e440188475b6a6323ba197e04979
959AI994 <115469462+959AI994@users.noreply.github.com>
2024-11-12 21:40:26 +0800

    feat:add code

---
+++ DeepLearningDemo/models/nn_conv2d.py
@@ -0,0 +1,47 @@
+import torch
+import torchvision
+
+
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import  datasets,transforms
+from torch.nn import Conv2d
+from torch.utils.tensorboard import SummaryWriter
+
+# 定义图像转换，例如将图像转换为Tensor并进行归一化
+process = transforms.Compose([
+    # transforms.Resize((256, 256)),  # 统一调整所有图像为 256x256
+    transforms.ToTensor(),
+    # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # 归一化为 [-1, 1] 范围
+])
+
+# 使用 ImageFolder 加载本地数据集
+test_data = datasets.ImageFolder(
+    root="E:/pythonProject/DeepLearningDemo/DeepLearningDemo/hymenoptera_data/train/",  # 本地数据集路径
+    transform=process  # 指定转换操作
+)
+dataloader = DataLoader(dataset=test_data,batch_size=4,shuffle=True,num_workers=0,drop_last=False)
+
+class Jingxin(nn.Module):
+    def __init__(self):
+        super(Jingxin,self).__init__()
+        self.conv1= Conv2d(in_channels=3,out_channels=6,kernel_size=3,stride=1,padding=0)
+
+    def forward(self,x):
+        x=self.conv1(x)
+        return  x
+writer=SummaryWriter("../logs")
+
+step=0
+
+for data in dataloader:
+    imgs,targets =data
+    output = Jingxin(imgs)
+    print(imgs.shape)
+    print(output.shape)
+
+    writer.add_image("input",imgs,step)
+
+    output= torch.reshape(output,(-1,-1,30,30))
+
+    step=step+1
