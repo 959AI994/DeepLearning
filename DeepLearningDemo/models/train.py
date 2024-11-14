4995b9f23d972c11198165cbca4802944b947d43
959AI994 <115469462+959AI994@users.noreply.github.com>
2024-11-14 14:52:52 +0800

    feat:搭建一个完整的神经网络，以CIFAR-10

--- DeepLearningDemo/models/train.py
+++ DeepLearningDemo/models/train.py
@@ -1,4 +1,7 @@
 import torchvision
+import torch
+from torch.utils.data import DataLoader
+from torch import nn
 
 # 准备训练数据集
 train_data=torchvision.datasets.CIFAR10(root="../data",train=True,transform=torchvision.transforms.ToTensor(),download=True)
@@ -10,3 +13,13 @@ train_data_size=len(train_data)
 test_data_size=len(test_data)
 print("训练数据集的长度为：{}".format(train_data_size))
 print("测试数据集的长度为：{}".format(test_data_size))
+
+# 利用Dataloader来加载数据集
+train_dataloader=DataLoader(train_data,batch_size=64)
+test_dataloader=DataLoader(test_data,batch_size=64)
+
+# 搭建一个名叫Jingxin的神经网络
+class Jingxin(nn.Moudle):
+    def __init__(self):
+        super(Jingxin, self).__init__()
+    def forward(self,x):
