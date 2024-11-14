ff6ef46a570e4351cc0d275c749d8e58b129799d
959AI994 <115469462+959AI994@users.noreply.github.com>
2024-11-14 19:46:59 +0800

    移除缓存中的大文件

--- DeepLearningDemo/test_tensorboard.py
+++
@@ -1,9 +0,0 @@
-# 在当前py文件的根目录（DeepLearningDemo对应的终端运行：tensorboard --logdir=logs） 即可。
-from torch.utils.tensorboard import SummaryWriter
-
-writer= SummaryWriter("logs")
-# 用tensorboard画一个一元二次函数
-for i in range(100):
-    writer.add_scalar("y=2x",2*i,i)
-
-writer.close()
